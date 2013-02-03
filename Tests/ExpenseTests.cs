5147ebc0998a31b1b803c442b41000ec22a923dd
Luke Sampson <me@lukesampson.com>
2013-02-04 09:58:55 +1000

    updated hasty API package, some testing

---
+++ Tests/ExpenseTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HastyAPI.FreshBooks.Tests {
+    public class ExpenseTests {
+        public void ListAllPage5() {
+            var res = Shared.NewCaller().Call("expense.list", (x) => { x.page = 5; });
+            //Console.WriteLine(res);
+        }
+
+        public void ListAllVendor() {
+            var res = Shared.NewCaller().Call("expense.list", (x) => { x.vendor = "Luke Sampson"; x.page = 2; });
+            //Console.WriteLine(res);
+        }
+
+        public void TestContractor() {
+            var res = Shared.NewCaller().Call("contractor.list");
+        }
+    }
+}
---
+++ Tests/InvoiceTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HastyAPI.FreshBooks.Tests {
+    public class InvoiceTests {
+        public void ListAll() {
+            var res = Shared.NewCaller().Call("invoice.list");
+            Console.Write(res.ToString());
+        }
+    }
+}
