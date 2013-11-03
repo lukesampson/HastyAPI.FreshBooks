a16e21dda8db50adf5a9ad0c631f14a4b17f381d
Kevin Moore <kevin.moore@vooban.com>
2013-11-03 18:58:46 -0500

    Patch to fix the Complex Object feature for create and update operation, which produces the wrong XML like this
    <request method="time_entry.create">
      <time_entry>[task_id, 999999][hours, 25]</time_entry>
    </request>
    
    Instead of
    
    <request method="time_entry.create">
      <time_entry>
        <task_id>999999</task_id>
        <hours>25</hours>
      </time_entry>
    </request>

--- FreshBooks.cs
+++ FreshBooks.cs
@@ -5,72 +5,102 @@ using System.Text;
 using System.Xml.Linq;
 using System.Dynamic;
 
-namespace HastyAPI.FreshBooks {
-	public class FreshBooks {
-		string Username { get; set; }
-		string Token { get; set; }
+namespace HastyAPI.FreshBooks
+{
+    public class FreshBooks
+    {
+        string Username { get; set; }
+        string Token { get; set; }
+        bool Verbose { get; set; }
 
-		public FreshBooks(string username, string token) {
-			Username = username;
-			Token = token;
-		}
+        public FreshBooks(string username, string token, bool verbose = true)
+        {
+            Username = username;
+            Token = token;
+            Verbose = verbose;
+        }
 
-		public dynamic Call(string method, Action<dynamic> getinputs = null) {
-			var req = CreateRequestXML(method, getinputs);
+        public dynamic Call(string method, Action<dynamic> getinputs = null)
+        {
+            var req = CreateRequestXML(method, getinputs);
 
-			System.Diagnostics.Debug.WriteLine("Sending request:\r\n" + req.ToString());
+            if (Verbose)
+                System.Diagnostics.Debug.WriteLine("Sending request:\r\n" + req.ToString());
 
-			var text = new APIRequest("https://" + Username + ".freshbooks.com/api/2.1/xml-in")
-				.WithBasicCredentials(Token, null)
-				.WithData(req.ToString())
-				.Post()
-				.EnsureStatus(200)
-				.Text;
+            var text = new APIRequest("https://" + Username + ".freshbooks.com/api/2.1/xml-in")
+                .WithBasicCredentials(Token, null)
+                .WithData(req.ToString())
+                .Post()
+                .EnsureStatus(200)
+                .Text;
 
-			var resp = XDocument.Parse(text);
-			System.Diagnostics.Debug.WriteLine("Received response:\r\n" + resp.ToString());
+            var resp = XDocument.Parse(text);
 
-			return resp.ToDynamic();
-		}
+            if (Verbose)
+                System.Diagnostics.Debug.WriteLine("Received response:\r\n" + resp.ToString());
 
-		private static XDocument CreateRequestXML(string method, Action<dynamic> getinputs) {
-			var xml = new XDocument();
+            return resp.ToDynamic();
+        }
 
-			var request = new XElement("request");
-			request.SetAttributeValue("method", method);
-			xml.Add(request);
+        private static XDocument CreateRequestXML(string method, Action<dynamic> getinputs)
+        {
+            var xml = new XDocument();
 
-			if(getinputs != null) {
-				dynamic inputs = new ExpandoObject();
-				getinputs(inputs);
+            var request = new XElement("request");
+            request.SetAttributeValue("method", method);
+            xml.Add(request);
 
-				foreach(var p in (inputs as IDictionary<string, object>)) {
-					request.Add(XMLElement(p.Key, p.Value));
-				}
-			}
+            if (getinputs != null)
+            {
+                dynamic inputs = new ExpandoObject();
+                getinputs(inputs);
 
-			return xml;
-		}
+                foreach (var p in (inputs as IDictionary<string, object>))
+                    request.Add(XMLElement(p.Key, p.Value));
+            }
 
-        private static XElement XMLElement(string name, object value) {
-            if(value == null) return new XElement(name, "");
+            return xml;
+        }
 
-            // special formatting (see http://developers.freshbooks.com/)
-            if(value is DateTime) value = ((DateTime)value).ToString("yyyy-MM-dd hh:mm:ss");
-            else if(value is bool) value = (bool)value ? "1" : "0";
+        private static XElement XMLElement(string name, object value)
+        {
+            if (value == null) return null;
 
-            if(value.GetType().IsValueType || value is string) {
+            if (value is DateTime) value = ((DateTime)value).ToString("yyyy-MM-dd hh:mm:ss");
+            else if (value is bool) value = (bool)value ? "1" : "0";
+
+            if (value.GetType().IsValueType || value is string)
+            {
                 return new XElement(name, value);
             }
 
-            // complex object
-            foreach(var prop in value.GetType().GetProperties()) {
-                var child = prop.GetValue(value, null);
-                return new XElement(name, XMLElement(prop.Name, child));
+            var potentialDynamic = (value as IDictionary<string, object>);
+            if (potentialDynamic == null)
+            {
+                // If the object is not a dynamic one, go the reflection way
+                var request = new XElement(name);
+
+                foreach (var prop in value.GetType().GetProperties())
+                {
+                    var child = prop.GetValue(value, null);
+                    return new XElement(name, XMLElement(prop.Name, child));
+                }
+
+                return request;
             }
+            else
+            {
+                // If the object is dynamic, process accordingly
+                var request = new XElement(name);
 
-            return new XElement(name, value);
+                foreach (var p in potentialDynamic)
+                {
+                    var newElement = XMLElement(p.Key, p.Value);
+                    request.Add(newElement);
+                }
+
+                return request;
+            }
         }
-
-	}
+    }
 }
